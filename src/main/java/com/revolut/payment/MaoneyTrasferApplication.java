279b8971efd3605a5799f6d383fe7ab6b106d18e
Kaushal Gour <Kaushal_Gour@epam.com>
2019-07-19 00:38:17 +0530

    initial commit

---
+++ src/main/java/com/revolut/payment/MaoneyTrasferApplication.java
@@ -0,0 +1,13 @@
+package com.revolut.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaoneyTrasferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaoneyTrasferApplication.class, args);
+	}
+
+}
